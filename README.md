fa3dbd780d8b389c68dae546d426626597806676
Enrique Plata <>
2023-02-26 16:12:21 +0100

    Adding ecr repository and policy

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # app-api-proxy
 NGINX proxy for a web-app
+
+## Description
+This project creates a NGINX proxy deploy in AWS ECR.
+
+## Requirements
+* AWS credentials that allows you to create ECR and IAM policy
+
+## Setup
+* Deploy the services in AWS
+    * In the deploy section of this repo run
+        *  `make tf-validate` 
+        *  `make tf-apply`
+    * This will create a new ECR repository and a policy
+* Create a new User in the AWS Management console and add the policy, the user only needs programatic access
+* Add the credentials to the github Actions
--- deploy/aws_ecr_repository.tf
+++ deploy/aws_ecr_repository.tf
@@ -6,3 +6,29 @@ resource "aws_ecr_repository" "app-api-proxy" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository_policy" "app-api-proxy-policy" {
+  repository = aws_ecr_repository.app-api-proxy.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:*"
+            ],
+            "Resource": "arn:aws:ecr:us-east-1:*:repository/app-api-proxy"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
